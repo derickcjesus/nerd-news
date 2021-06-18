8c64eee6645d2a79facf199909299f0de7a96976
Derick <derickcjesus@gmail.com>
2021-06-17 22:44:44 -0300

    cria página de listagem de notícias

---
+++ src/pages/ListNews/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Header from '../../components/Header';
+import NewsCards from '../../components/NewsCards';
+
+export default function ListNews() {
+    return (
+        <>
+            <Header />
+            <NewsCards />
+        </>
+    );
+}
