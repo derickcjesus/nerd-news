3fd05422b0709e0128e759cca3c4f0f013734cc6
Derick <derickcjesus@gmail.com>
2021-06-20 15:36:49 -0300

    refatora o componente News Card

---
+++ src/components/NewsCard/index.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import "./styles.css";
+
+const NewsCard = ({props}) => {
+  return (
+    <div className="cards">
+      <img
+        src={props.imagem}
+        alt={props.imagem}
+      />
+      <div className="container">
+        <h1>
+          {props.titulo}
+        </h1>
+        <p>
+          {props.descricao}
+        </p>
+        <a href="omelete.com">LER MAIS</a>
+      </div>
+    </div>
+  );
+} 
+
+export default NewsCard;
