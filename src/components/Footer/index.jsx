315f8c8f6e69b8ce9b2300655b647e0d9c9843ca
Derick <derickcjesus@gmail.com>
2021-06-21 10:08:12 -0300

    cria componente Footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './styles.css';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>
+            © 2021  Omelete - Todos os direitos reservados.
+            </p>
+        </footer>
+    );
+}
+
+export default Footer;
