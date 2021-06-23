a0fea87f61b65f3ed26ebf250b8a5130c2ec4a0d
Derick <derickcjesus@gmail.com>
2021-06-23 15:07:21 -0300

    mostra a notícia clicada

--- src/pages/News/index.jsx
+++ src/pages/News/index.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "../../services/api";
+import { useRouteMatch } from "react-router-dom";
 
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
@@ -6,32 +8,31 @@ import Footer from "../../components/Footer";
 import "./styles.css";
 
 const News = () => {
+  const [noticia, setNoticia] = useState([]);
+
+  const { params } = useRouteMatch();
+
+  useEffect(() => {
+    api.get(`/?id=${params.id}`).then((response) => {
+      setNoticia(response.data);
+    });
+  }, [params.id]);
+
+  const findNew = noticia.find((noticia) => noticia.id == params.id);
+
   return (
     <>
       <Header />
-
-      <main className="container">
-        <h1>
-          Tico e Teco: veja a abertura e pôster oficiais de nova animação do
-          Disney+
-        </h1>
-        <picture>
-          <img
-            src="https://cdn.ome.lt/kourIIJKMmbc2HIb_krfrmLHzpg=/fit-in/837x500/smart/uploads/conteudo/fotos/Poster_Oficial_f3KpTOM.jpg"
-            alt="tico e teco"
-          />
-        </picture>
-
-        <p>
-          Tico e Teco estão de volta. Desta vez, a dupla de esquilos vai se
-          aventurar pelo Disney+, em novos episódios que estreiam em julho na
-          plataforma de streaming do Mickey Mouse. Acima, você pode conferir a
-          nova abertura da animação. Composto por três histórias de sete
-          minutos, cada episódio vai seguir a dupla tentando viver
-          tranquilamente nos parques da cidade. Os dois roedores também estarão
-          acompanhados por Pluto, Butch e outros personagens icônicos da Disney.
-        </p>
-      </main>
+      {findNew && (
+        <main className="container">
+          <h1>{findNew.titulo}</h1>
+          <picture>
+            <img src={findNew.imagem} alt="imagem-noticia" />
+          </picture>
+          <h4>{findNew.publicacao}</h4>
+          <p>{findNew.texto}</p>
+        </main>
+      )}
 
       <Footer />
     </>
