31c30d6b16d32a8f639b67bad174ea16673d1af7
Derick <derickcjesus@gmail.com>
2021-06-23 14:55:53 -0300

    troca tag "a" por "span"

--- src/components/NewsCard/index.jsx
+++ src/components/NewsCard/index.jsx
@@ -2,24 +2,17 @@ import React from "react";
 
 import "./styles.css";
 
-const NewsCard = ({props}) => {
+const NewsCard = ({ props }) => {
   return (
     <div className="cards">
-      <img
-        src={props.imagem}
-        alt={props.imagem}
-      />
+      <img src={props.imagem} alt={props.titulo} />
       <div className="container-news-card">
-        <h1>
-          {props.titulo}
-        </h1>
-        <p>
-          {props.descricao}
-        </p>
-        <a href="omelete.com">LER MAIS</a>
+        <h1>{props.titulo}</h1>
+        <p>{props.descricao}</p>
+        <span>LER MAIS</span>
       </div>
     </div>
   );
-} 
+};
 
 export default NewsCard;
